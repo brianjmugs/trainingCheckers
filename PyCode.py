7b602bf34bf2df4d641b03073a28aedb6a9de115
brianjmugs <brianjmulligan@hotmail.com>
2022-11-13 11:13:14 +0000

    Added some training data and a text file

---
+++ PyCode.py
@@ -0,0 +1,8 @@
+#import the training text file, strip characters. Leave only the movements and weights. 
+#Doing this so I can over time see which movements I'm missing and work on them throughout the month/year
+f = open('7th Training.txt', 'r')
+content = f.read()
+content = content.replace("- ","")
+content = content.replace("[","")
+content = content.replace("]","")
+print(content)
